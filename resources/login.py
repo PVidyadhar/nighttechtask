8b0739ea7a9405dd715288e72f47a31638351ef1
Vidyadhar Pogul <pogul.vidyadhar@gmail.com>
2020-06-12 16:33:05 +0530

    Completed task

--- resources/login.py
+++ resources/login.py
@@ -7,14 +7,14 @@ class Login(Resource):
         data = json.loads(data) 
         image=request.files.get('file','')
         #image.save(f'''D:/{data['name']}.jpg''')
-        #if (open(f'''D:/{data['name']}.jpg''',"rb", buffering=0) is None):
-            #return {"message":"image cannot be empty"},201'''
+        if (image is None):
+            return {"message":"image cannot be empty"},201
         if data['name'] == "":
             return {"message":"name cannot be empty"},201
         if data['emailid'] == "":
             return {"message":"emailid cannot be empty"},201
 
-        #try:
-        return f"""Your name is {data['name']} and email id is {data['emailid']} and image is received succesully"""
-        #except:
-        #return {"message":"There was an error while returning data."},500
+        try:
+            return f"""Your name is {data['name']} and email id is {data['emailid']} and image is received succesully"""
+        except:
+            return {"message":"There was an error while returning data."},500
