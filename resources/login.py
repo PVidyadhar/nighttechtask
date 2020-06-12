59528e41da4446d253c2c94c0a706cfa34817295
Vidyadhar Pogul <pogul.vidyadhar@gmail.com>
2020-06-12 16:54:20 +0530

    Completed task

--- resources/login.py
+++ resources/login.py
@@ -1,12 +1,18 @@
 from flask_restful import Resource
 from flask import request,json
+import shutil
 
 class Login(Resource):
     def post(self):
         data=request.form['data']
         data = json.loads(data) 
         image=request.files.get('file','')
+        filename=f'''D:/{data['name']}.jpg'''
+        #if image.status_code == 200:
+        #image.raw.decode_content = True
         #image.save(f'''D:/{data['name']}.jpg''')
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(image, f)
         if (image is None):
             return {"message":"image cannot be empty"},201
         if data['name'] == "":
