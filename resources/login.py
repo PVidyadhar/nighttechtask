358850e104005fe91af16afa3d1457dba54c177f
Vidyadhar Pogul <pogul.vidyadhar@gmail.com>
2020-06-12 17:05:15 +0530

    Completed task

--- resources/login.py
+++ resources/login.py
@@ -1,18 +1,18 @@
 from flask_restful import Resource
 from flask import request,json
-import shutil
+#import shutil
 
 class Login(Resource):
     def post(self):
         data=request.form['data']
         data = json.loads(data) 
         image=request.files.get('file','')
-        filename=f'''{data['name']}.jpg'''
+        #filename=f'''{data['name']}.jpg'''
         #if image.status_code == 200:
         #image.raw.decode_content = True
         #image.save(f'''D:/{data['name']}.jpg''')
-        with open(filename,'wb') as f:
-            shutil.copyfileobj(image, f)
+        #with open(filename,'wb') as f:
+        #shutil.copyfileobj(image, f)'''
         if (image is None):
             return {"message":"image cannot be empty"},201
         if data['name'] == "":
@@ -21,6 +21,6 @@ class Login(Resource):
             return {"message":"emailid cannot be empty"},201
 
         try:
-            return f"""Your name is {data['name']} and email id is {data['emailid']} and image is received succesully"""
+            return f"""Your name is {data['name']} and email id is {data['emailid']} and image is received succefully"""
         except:
             return {"message":"There was an error while returning data."},500
